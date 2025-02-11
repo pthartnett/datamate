869d8a2771f6c9c9ed10f4005d7bed6ea20f944d
pthartnett <pthartnett@gmail.com>
2025-02-11 17:00:46 -0500

    add in tests for dataset class

--- datamate/tests/test_dataset.py
+++ datamate/tests/test_dataset.py
@@ -20,3 +20,46 @@ def test_dataframe_no_named_cols_input():
     assert set(ds.df.columns)=={0,1,2}
     assert ds.y is not None
     assert ds.x is not None
+
+def test_numpy_array():
+    df = np.array([[1,2,3,4],[2,3,5,7],[3,3,None,5]])
+    ds = dataset(df, dependent_var = 0, independent_vars=[1,2])
+
+    assert ds.df.shape==(3,3)
+    assert set(ds.df.columns)=={0,1,2}
+    assert ds.y is not None
+    assert ds.x is not None
+    assert ds.y.shape == (3, 1)
+    assert ds.x.shape == (3, 2) 
+
+def test_list():
+    df = [[1,2,3,4],[2,3,5,7],[3,3,None,5]]
+    ds = dataset(df, dependent_var = 0, independent_vars=[1,2])
+
+    assert ds.df.shape==(3,3)
+    assert set(ds.df.columns)=={0,1,2}
+    assert ds.y is not None
+    assert ds.x is not None
+    assert ds.y.shape == (3, 1)
+    assert ds.x.shape == (3, 2) 
+
+def test_column_selection():
+    df = pd.DataFrame([[1,2,3,4],[2,3,5,7],[3,3,None,5]])
+    ds = dataset(df, dependent_var = 0, independent_vars=[1,2])
+
+    assert "d" not in ds.df.columns
+
+def test_summary():
+    df = pd.DataFrame([[1,2,3,4],[2,3,5,7],[3,3,None,5]],columns=['a','b','c','d'])
+    ds = dataset(df, dependent_var = 'a', independent_vars=['b','c'])
+    
+    summary = ds.summary()
+    
+    assert summary["num_rows"] == 3
+    assert summary["num_columns"] == 3
+    assert summary["dependent_var"] == ['a']
+    assert set(summary["independent_vars"]) == {'b','c'}
+    assert summary["original_format"] == pd.DataFrame
+    assert summary["analysis_type"] == "ols"
+    assert summary["num_missing"] == 1
+    assert summary["columns_with_missing"] == {"c":1}
